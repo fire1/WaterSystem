94436e348c9311a0fac8bdeb0de8e4033cadea93
fire1@abv.bg <fire1@abv.bg>
2024-02-19 09:25:23 +0200

    Refactoring
    Saperating read from rule

---
+++ Master/lib/Read.h
@@ -0,0 +1,201 @@
+#ifndef Read_h
+#define Read_h
+
+#include "Glob.h"
+
+class Read {
+
+private:
+
+  SoftwareSerial com;
+  AsyncDelay timerIdle;
+  AsyncDelay timerWork;
+
+  struct LevelSensorAverage {
+    uint8_t index = 0;
+    uint32_t average = 0;
+    bool done = false;
+  };
+
+  //
+  // Average from sensors
+  LevelSensorAverage
+    sensorWell,
+    sensorMain;
+
+  bool isWorkRead = false;
+
+  //
+  // Final average valuse from sensors
+  uint8_t well;
+  uint8_t main;
+
+public:
+  Read()
+    : com(pinMainRx, -1) {}
+
+  void begin() {
+    //
+    // UAR begins
+    com.begin(BaudSlaveRx);
+    //
+    //
+    timerIdle.start(LevelRefreshTimeIdle, AsyncDelay::MILLIS);
+    //
+    // Well tank sensor pins
+    pinMode(pinWellEcho, INPUT);
+    pinMode(pinWellSend, OUTPUT);
+    digitalWrite(pinWellSend, LOW);
+    //
+    // Main tank Slave pins
+    pinMode(pinMainPower, OUTPUT);
+    digitalWrite(pinMainPower, LOW);
+  }
+
+
+  void hark() {
+    this->readAtIdle();
+    this->readAtWork();
+  }
+
+  //
+  // Ouput to pass  information from this methods
+  //
+  uint8_t getWellLevel() {
+    return this->well;
+  }
+
+  uint8_t getMainLevel() {
+    return this->main;
+  }
+
+  //
+  // Sorter period for sensors
+  void startWorkRead() {
+    if (!this->isWorkRead) this->startShortReadTimer();
+    this->isWorkRead = true;
+  }
+
+  void stopWorkRead() {
+    this->isWorkRead = false;
+  }
+
+private:
+
+  void startShortReadTimer() {
+    timerWork.start(LevelRefreshTimeWork, AsyncDelay::MILLIS);
+  }
+
+  //
+  // Read well tank
+  void readWell() {
+
+    if (this->sensorWell.index < LevelSensorReads) {
+
+      digitalWrite(pinWellSend, LOW);
+      delayMicroseconds(2);
+      digitalWrite(pinWellSend, HIGH);
+      delayMicroseconds(10);
+      digitalWrite(pinWellSend, LOW);
+
+      unsigned long duration = pulseIn(pinWellEcho, HIGH, 130000);  // read pulse with timeout for ~150cm
+
+      // Check for timeout
+      if (duration == 0) {
+        //     Serial.println("Timeout error: Sensor WELL reading exceeds range");
+        // Handle timeout (e.g., set distance to maximum or other logic)
+        return;
+      }
+
+      float distance = (duration * .0343) / 2;
+
+
+      // Store distance reading
+      sensorWell.average += distance;
+      sensorWell.index++;  // Increment index for next reading
+
+    } else {
+      this->well = sensorWell.average / sensorWell.index;
+      sensorWell.index = 0;
+      sensorWell.average = 0;
+      sensorWell.done = true;
+      dbg(F("Well tank average value: "));
+      dbg(this->well);
+      dbgLn();
+    }
+  }
+
+  //
+  // Read Main tank using slave MCU
+  //
+  // OneWire setup (Power/Serial)
+  // The OneWire Serial is desined to provide a power and
+  //  communication from the serial slave sensor.
+  // https://forum.arduino.cc/t/jsn-sr04t-2-0/456255/10
+  void readMain() {
+    if (sensorMain.index < LevelSensorReads) {
+      if (digitalRead(pinMainPower)) {
+        digitalWrite(pinLed, LOW);
+
+        // Check for available data and read value
+        if (com.available()) {
+          digitalWrite(pinLed, HIGH);
+
+          uint8_t currentValue = com.read();
+
+          // Store value in the sensorMain struct
+          sensorMain.average += currentValue;
+          sensorMain.index++;  // Increment index for next reading
+
+          dbg(F("RX: "));
+          dbgLn(currentValue);  // Print current value for debugging
+          digitalWrite(pinLed, LOW);
+        }
+      } else {
+        digitalWrite(pinMainPower, HIGH);
+      }
+    } else {
+      this->main = sensorMain.average / sensorMain.index;
+      sensorMain.index = 0;
+      sensorMain.average = 0;
+      sensorMain.done = true;
+      dbg(F("Main tank average value: "));
+      dbg(this->main);
+      dbgLn();
+      digitalWrite(pinMainPower, LOW);
+    }
+  }
+
+  void readAtIdle() {
+
+    if (!sensorWell.done) this->readWell();
+
+    if (!sensorMain.done) this->readMain();
+
+    //
+    // When we idle pumps just will check levels
+    if (timerIdle.isExpired()) {
+      sensorWell.done = false;
+      sensorMain.done = false;
+      timerIdle.repeat();
+    }
+  }
+  //
+  // When pumps are running we will read in short periods
+  void readAtWork() {
+    //
+    // In case something is runned manually
+    if ((ctrlMain.isOn() || ctrlWell.isOn()) && !this->isWorkRead) {
+      this->isWorkRead = true;
+      this->startShortReadTimer();
+    }
+
+    if (this->isWorkRead && timerWork.isExpired()) {
+      sensorWell.done = false;
+      sensorMain.done = false;
+      timerWork.repeat();
+    }
+  }
+};
+
+#endif
